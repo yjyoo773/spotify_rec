ea9cfc9671ab9c3124d9e9a5853f9dd71eb5f748
Ellis Yoo <yjoo.eyoo@gmail.com>
2020-12-30 19:53:58 -0800

    commit2

--- test.py
+++ test.py
@@ -6,9 +6,6 @@ from connect_api import get_id_name,create_necessary_outputs, visualize_songs
 from playlist_vector import playlist_vector
 from create_rec import create_playlist_rec
 
-
-
-
 spotify_df = pd.read_csv("data.csv")
 data_w_genre = pd.read_csv("data_w_genres.csv")
 
